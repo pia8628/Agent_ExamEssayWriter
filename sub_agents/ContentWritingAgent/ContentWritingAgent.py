872d8a637256211cd656eddce634dc00ac0caa40
pia8628 <pia8628@gmail.com>
2025-11-25 00:05:33 +0800

    update google search

--- sub_agents/ContentWritingAgent/ContentWritingAgent.py
+++ sub_agents/ContentWritingAgent/ContentWritingAgent.py
@@ -7,7 +7,8 @@ search_agent = Agent(
     model="gemini-2.5-flash",
     name="search_agent",
     instruction="Use Google Search to find up-to-date, reliable sources and return grounded findings.",
-    tools=[google_search],  
+    tools=[google_search],
+    output_key="SearchResults",  
 )
 
 web_search_tool = AgentTool(agent=search_agent)
@@ -17,12 +18,14 @@ ContentWritingAgent = Agent(
     name="ContentWritingAgent",
     description="輸入OutlineGenerationAgent產出的大綱{EssayOutline}，以及TopicAnalysisAgent產出的題目分析報告{TopicAnalysis}，生成符合國考作文要求的文章內容",
     instruction="""
+    輸入OutlineGenerationAgent產出的大綱{EssayOutline}，以及TopicAnalysisAgent產出的題目分析報告{TopicAnalysis}
     處理過程
     1. 逐一處理大綱中的每個要點，生成完整段落。
     2. 語氣口語一點，不要太過生硬。
-    3. 如果有需要，使用google_search工具來查找相關資訊以豐富內容。
+    3. 如果有需要，使用web_search_tool工具來查找相關資訊以豐富內容。
     4. 注意總字數限制不要超過700字。
     5. 輸出：數個獨立的、完整的文章段落，每個段落不需要標題。
+    如果使用者確認沒問題，請把交給ReviewAndPolishAgent進行下一步的審核和潤色。
     """,
     output_key="EssayContent",
     tools=[web_search_tool],
--- sub_agents/OutlineGenerationAgent/OutlineGenerationAgent.py
+++ sub_agents/OutlineGenerationAgent/OutlineGenerationAgent.py
@@ -12,7 +12,8 @@ search_agent = Agent(
     model="gemini-2.5-flash",
     name="search_agent",
     instruction="Use Google Search to find up-to-date, reliable sources and return grounded findings.",
-    tools=[google_search],  
+    tools=[google_search], 
+    output_key="SearchResults",
 )
 
 web_search_tool = AgentTool(agent=search_agent)
@@ -22,6 +23,7 @@ OutlineGenerationAgent = Agent(
     name="OutlineGenerationAgent",
     description="輸入TopicAnalysisAgent產出的題目分析報告{TopicAnalysis}，生成符合國考作文要求的文章大綱",
     instruction="""
+    輸入TopicAnalysisAgent產出的題目分析報告{TopicAnalysis}
     處理過程
     1. 根據題目分析報告，確定文章的主題和重點
     2. 設計符合國考作文「起、承、轉、合」的大綱
@@ -38,10 +40,11 @@ OutlineGenerationAgent = Agent(
             - 承：支持論點的理由和證據，如果相關，可加入熱門議題如：AI、氣候變遷、人性反思等
             - 轉：反駁可能的反對意見
             - 合：總結論點，強調立場
-        如果內容有關於時事，請使用google_search工具查核事實
+        重要！！如果內容有關於時事，請使用web_search_tool工具查核事實
     5. 至多不要超過5段，並為每個部分分配適當的字數比例，例如：起(15%)、承(35%)、轉(30%)、合(20%)。
     6. 確保大綱邏輯清晰且連貫
     7. 輸出：一個結構化大綱，每個部分都有明確的主題。
+    如果使用者確認大綱沒有問題，請交給ContentWritingAgent進行下一步的內容撰寫。
     """,
 
     tools=[web_search_tool],
